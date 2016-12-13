1d2bfa804e04d6911cba3a08977da90aa0526dd5
LAPTOP-4N6KCO21\taniasekhon <tania.sekhon@dishq.in>
2016-12-13 14:16:06 +0530

    splash screen implementation

--- app/src/main/java/version1/dishq/dishq/ui/SplashActivity.java
+++ app/src/main/java/version1/dishq/dishq/ui/SplashActivity.java
@@ -3,6 +3,7 @@ package version1.dishq.dishq.ui;
 import android.os.Bundle;
 
 import version1.dishq.dishq.BaseActivity;
+import version1.dishq.dishq.R;
 
 /**
  * Created by dishq on 13-12-2016.
@@ -14,7 +15,7 @@ public class SplashActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setContentView(R.layout.activity_splash);
     }
 
 }
--- app/src/main/java/version1/dishq/dishq/util/DishqApplication.java
+++ app/src/main/java/version1/dishq/dishq/util/DishqApplication.java
@@ -1,10 +1,22 @@
 package version1.dishq.dishq.util;
 
 
+import android.content.Context;
+import android.support.multidex.MultiDex;
+
 /**
  * Created by dishq on 13-12-2016.
  * Package name version1.dishq.dishq.
  */
 
-public final class DishqApplication{
+public final class DishqApplication extends android.support.multidex.MultiDexApplication{
+
+    public static DishqApplication application;
+
+    @Override
+    protected void attachBaseContext(Context base)
+    {
+        super.attachBaseContext(base);
+        MultiDex.install(application);
+    }
 }
