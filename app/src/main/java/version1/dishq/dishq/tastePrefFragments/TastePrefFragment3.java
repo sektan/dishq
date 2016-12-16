99164444501ed26f1b4f8751bdd835038200f8f7
LAPTOP-4N6KCO21\taniasekhon <tania.sekhon@dishq.in>
2016-12-17 00:00:16 +0530

    onboarding screens part 2

--- app/src/main/java/version1/dishq/dishq/tastePrefFragments/TastePrefFragment3.java
+++ app/src/main/java/version1/dishq/dishq/tastePrefFragments/TastePrefFragment3.java
@@ -4,12 +4,19 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.View;
 
+import com.wefika.flowlayout.FlowLayout;
+
+import version1.dishq.dishq.modals.FavCuisinesModal;
+
 /**
  * Created by dishq on 16-12-2016.
  * Package name version1.dishq.dishq.
  */
 
 public class TastePrefFragment3 extends Fragment implements View.OnClickListener {
+
+    private FavCuisinesModal favCuisinesModal;
+    private FlowLayout favCuisineContainer;
 
     public static TastePrefFragment3 newInstance(String text) {
         TastePrefFragment3 f = new TastePrefFragment3();
